e7a0dfa6c449cf25e5b63343b9f4603e126194cc
Nicolas Paul <nicolas.paul@outlook.com>
2024-07-10 11:56:32 -0400

    update

--- Helpers/auth.py
+++ Helpers/auth.py
@@ -1,12 +1,22 @@
 import requests
 import json
 
-def get_auth_endpoint_response(self):
-    url = f'{self.url}' + 'auth'
+def get_auth_endpoint_response_good_auth(self):    
     payload = json.dumps({
         "username": self.username,
         "password": self.password
-    })
+    })    
+    return post_to_auth_endpoint(self, payload)
+
+def get_auth_endpoint_response_custom_credentials(self, username, password):
+    payload = json.dumps({
+        "username": username,
+        "password": password
+    })    
+    return post_to_auth_endpoint(self, payload)
+
+def post_to_auth_endpoint(self, payload):
+    url = f'{self.url}' + 'auth'
     headers = {
     'Content-Type': 'application/json'
     }
--- test_auth_endpoints.py
+++ test_auth_endpoints.py
@@ -10,7 +10,22 @@ class TestAuthEndpoints:
         cls.username = configs['auth']['username']
         cls.password = configs['auth']['password']
     
-    def test_GIVEN_good_credentials_WHEN_sent_to_auth_api_THEN_200_is_returned(self): 
-        response = auth.get_auth_endpoint_response(self)
+    def test_GIVEN_good_credentials_WHEN_sent_to_auth_api_THEN_200_is_returned_and_no_error_returned(self): 
+        response = auth.get_auth_endpoint_response_good_auth(self)
+        assert response.status_code == 200
+        assert response.text != '{"reason":"Bad credentials"}'
+
+    def test_GIVEN_bad_username_credential_WHEN_sent_to_auth_api_THEN_200_is_returned_and_error_is_returned(self): 
+        response = auth.get_auth_endpoint_response_custom_credentials(self, 'Nick', self.password)
+        assert response.text == '{"reason":"Bad credentials"}'
+        assert response.status_code == 200
+    
+    def test_GIVEN_bad_password_credential_WHEN_sent_to_auth_api_THEN_200_is_returned_and_error_is_returned(self): 
+        response = auth.get_auth_endpoint_response_custom_credentials(self, self.username, 'Paul')
+        assert response.text == '{"reason":"Bad credentials"}'
         assert response.status_code == 200
 
+    def test_GIVEN_bad_credentials_WHEN_sent_to_auth_api_THEN_200_is_returned_and_error_is_returned(self): 
+        response = auth.get_auth_endpoint_response_custom_credentials(self, 'Nick', 'Paul')
+        assert response.text == '{"reason":"Bad credentials"}'
+        assert response.status_code == 200
