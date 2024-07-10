e2df714055737efde998f78cb676e5936e085add
Nicolas Paul <nicolas.paul@outlook.com>
2024-07-10 11:37:16 -0400

    final updates before public

--- test_auth_endpoints.py
+++ test_auth_endpoints.py
@@ -2,7 +2,7 @@ import Helpers.base as base
 import Helpers.auth as auth
 
 
-class TestExample:
+class TestAuthEndpoints:
     @classmethod
     def setup_class(cls):
         configs = base.read_json_to_dict('config_test.json')
@@ -10,7 +10,7 @@ class TestExample:
         cls.username = configs['auth']['username']
         cls.password = configs['auth']['password']
     
-    def test_example(self): 
+    def test_GIVEN_good_credentials_WHEN_sent_to_auth_api_THEN_200_is_returned(self): 
         response = auth.get_auth_endpoint_response(self)
         assert response.status_code == 200
 
